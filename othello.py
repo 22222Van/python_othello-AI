d8174fae36577a1f43f76cc7742ad4fbc9327f37
QIAN Jiaye <qianjy2022@shanghaitech.edu.cn>
2025-01-11 17:44:47 +0800

    Revert "Modified APIs."
    This reverts commit ac5e0778275c1609d0e92657b0a8118fc1d4390f.

--- othello.py
+++ othello.py
@@ -3,9 +3,11 @@ from agents import BaseAgent, Player
 from game import Game
 from game_state import GameStatus
 from utils import *
-import random
+
 
 def seed_everything(seed: int) -> None:
+    import random
+
     random.seed(seed)
     # np.random.seed(seed)
     # torch.manual_seed(seed)
@@ -31,13 +33,6 @@ def get_agent_from_cli(cli_list: list[str], color: ColorType) -> BaseAgent:
     return BaseAgent.registry[agent_name.lower()](color, **kwargs)
 
 
-def play_game(args):
-    agent1, agent2, graphics, debug, seed = args
-    seed_everything(seed)
-    game = Game(agent1, agent2, graphics, debug)
-    return game.start()
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='''
@@ -54,21 +49,20 @@ Add copyright information here.
     )
     parser.add_argument('--no-graphics', '-q', action='store_true')
     parser.add_argument('--debug', action='store_true')
-    parser.add_argument('--total-games', '-n', type=int, default=1)
+    parser.add_argument('--total-games', '-N', type=int, default=1)
     parser.add_argument('--seed', default=None, type=int)
 
     args = parser.parse_args()
 
     seed = args.seed
-    if seed is None:
-        seed = random.getrandbits(32)
+    if seed is not None:
+        seed_everything(seed)
 
     agent1 = get_agent_from_cli(args.agent1, 'B')
     agent2 = get_agent_from_cli(args.agent2, 'W')
 
     total_games: int = args.total_games
     graphics: bool = not args.no_graphics
-    debug: bool = args.debug
 
     if (
         total_games > 1 and
@@ -78,9 +72,8 @@ Add copyright information here.
         graphics = False
 
     if total_games == 1:
-        final_state = play_game(
-            (agent1, agent2, graphics, debug, seed)
-        )
+        game = Game(agent1, agent2, graphics, args.debug)
+        final_state = game.start()
         black_count, white_count = final_state.black_white_counts
         print(f'{final_state}')
         print(f'Black {black_count}-{white_count} White')
@@ -94,7 +87,7 @@ Add copyright information here.
 
         pbar = tqdm(range(total_games), desc="0-0-0")
         for _ in pbar:
-            game = Game(agent1, agent2, graphics, debug)
+            game = Game(agent1, agent2, graphics, args.debug)
             final_status = game.start().status
             if final_status == GameStatus.BLACK_WIN:
                 black_wins += 1
