88c9ce0f74049bd74bfc1a0ec2a8cba807a9ef05
andy <liyx12022@shanghaitech.edu.cn>
2025-01-16 14:04:45 +0800

    initial completed approx Q feature ver01

--- approxQ_feature.py
+++ approxQ_feature.py
@@ -3,8 +3,8 @@ from game_state import StateStatus, GameState
 
 
 # the same as CountOwnPieces.evaluate() in heuristics.py
-def B_W_counts(game_state: GameState, color: PlayerColorType) -> float:
-    bn, wn = game_state.black_white_counts
+def B_W_counts(state: GameState, color: PlayerColorType) -> float:
+    bn, wn = state.black_white_counts
     if color == BLACK:
         return bn  # count of black
     else:
@@ -12,12 +12,70 @@ def B_W_counts(game_state: GameState, color: PlayerColorType) -> float:
 
 
 # stable pieces (i.e. position with chess) that will not be flipped
-def is_stable():
-    1  # TBD
+def is_stable(state: GameState, color: PlayerColorType, i: int, j: int) -> bool:
+    grid = state._grid
+    opponent_color = -color
+
+    if (i, j) in CORNERS:
+        return True
+
+    directions = [(0, 1), (1, -1), (1, 0), (1, 1)]  # four basic directions
+    """     
+    When positions of certain 2 opposite direction of (x, y) contains the following content:
+        1. there's an EMPTY in one direction, and an opponent_color in another;
+        2. there's an EMPTY in both directions;
+    Then this (x, y) is not stable, it's color could be changed. 
+    """
+    s1 = ""
+    s2 = ""
+
+    for dx, dy in directions:
+
+        x, y = i, j
+        for _ in range(BOARD_WIDTH):
+            x += dx
+            y += dy
+
+            if not (0 <= x < BOARD_WIDTH and 0 <= y < BOARD_WIDTH):
+                break  # Out of bounds
+
+            if grid[x][y] == opponent_color:
+                s1 = "op"
+            elif grid[x][y] == EMPTY:
+                s1 = "empty"
+                break
+
+        x, y = i, j
+        for _ in range(BOARD_WIDTH):
+            x -= dx
+            y -= dy
+
+            if not (0 <= x < BOARD_WIDTH and 0 <= y < BOARD_WIDTH):
+                break  # Out of bounds
+
+            if grid[x][y] == opponent_color:
+                s2 = "op"
+            elif grid[x][y] == EMPTY:
+                s2 = "empty"
+                break
+
+        if {s1, s2} == {"op", "empty"} or {s1, s2} == {"empty", "empty"}:
+            return False
+
+    return True
 
 
-def num_stables():
-    2  # TBD
+def n_stables(state: GameState, color: PlayerColorType) -> int:
+    # stables = []
+    n = 0
+    for i in range(BOARD_WIDTH):
+        for j in range(BOARD_WIDTH):
+            if state._grid[i][j] == color:
+                if is_stable(state._grid, color, i, j):
+                    # stables.append((i, j))  # Store the position of stable discs
+                    n += 1
+    # return stables
+    return n
 
 
 def get_features(state: GameState, action: PointType) -> Counter:
@@ -56,23 +114,23 @@ def get_features(state: GameState, action: PointType) -> Counter:
 
     features = Counter()  # all values are 0 defaultly
 
-    features["bias"] = 1.0
+    features["bias"] = 1.0  # may adjust?
 
     # if the action is to put my chess on a corner, then that's great!
     if action in CORNERS:
-        features["is_corner"] = 4.0
+        features["is_corner"] = 4.0  # may adjust?
 
     # threat that opponent would take the corner,
     # kind of overlap with CORNER_NEIGHBOURS
     for a in opponent_legal_actions:
         if a in CORNERS:
-            features["corner_threat"] -= 2
+            features["corner_threat"] -= 2  # may adjust?
 
     # if the action is to put my chess on a position next to corner,
     # then that's bad, which would probably make me lose the corner!
     if action in star_positions:
         if features["corner_threat"] != 0:
-            features["is_corner_neighbour"] = -1.0
+            features["is_corner_neighbour"] = -1.0  # may adjust?
     # the rest positions on edges are "subopitimal"
     elif (
         action_x == 0
@@ -80,22 +138,26 @@ def get_features(state: GameState, action: PointType) -> Counter:
         or action_y == 0
         or action_y == BOARD_WIDTH - 1
     ):
-        features["is_edge"] = 1.0
+        features["is_edge"] = 1.0  # may adjust?
 
     # evaluate the changes that the action would make on my state
     features["flips"] = B_W_counts(opponent_state, color) - B_W_counts(state, color)
     # may adjust the feature:
-    features["flips"] = features["flips"] / 16.0
+    features["flips"] = features["flips"] / 20.0
 
+    # Our control over the chess board:
     # opponent has action(s) to take
     if opponent_state.legal_actions:
-        features["mobility_ratio"] = len(state.legal_actions) / len(
-            opponent_state.legal_actions
-        )
+        features["mobility_ratio"] = n_actions / n_oppoent_actions
         # may adjust the feature:
         features["mobility_ratio"] = features["mobility_ratio"] / 16.0
     # opponent has no action to take
     else:
-        features["mobility_ratio"] = 1.0
+        features["mobility_ratio"] = 1.0  # may adjust?
+
+    # Addition od positions of my color that won't be changed
+    features["stable_add"] = n_stables(opponent_state, color) - n_stables(state, color)
+    # may adjust the feature:
+    features["stable_add"] = features["stable_add"] / 16.0
 
     return features
